15b70f38b8696b25fc6ce0829c52a69932b314fe
xixi <i@hexilee.me>
2022-07-31 23:56:23 +0800

    add io worker
    Signed-off-by: xixi <i@hexilee.me>

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,32 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
-use std::sync::mpsc::{channel, Sender, TryRecvError};
 use std::thread::JoinHandle;
-use std::time::Instant;
+// use std::time::Instant;
 
 use anyhow::{anyhow, Result};
-use tokio::sync::oneshot;
+use bytes::Bytes;
+use futures::executor::block_on;
+use tokio::sync::{oneshot, mpsc};
+use tokio::task;
 
 #[cfg(test)]
 mod test;
 
+#[derive(Debug)]
 enum Message {
     Exit,
     Write {
-        data: Vec<u8>,
+        data: Bytes,
         waker: oneshot::Sender<()>,
     },
 }
 
 pub struct Logger {
-    sender: Sender<Message>,
-    worker_handler: Option<JoinHandle<()>>,
+    batch_sender: mpsc::UnboundedSender<Message>,
+    batch_worker: Option<JoinHandle<()>>,
+    io_worker: Option<JoinHandle<()>>,
+    io_sender: mpsc::UnboundedSender<Message>,
 }
 
 impl Logger {
@@ -36,33 +41,56 @@ impl Logger {
             .custom_flags(libc::O_DIRECT)
             .open(path)?;
         let max_buffer = max_buffer_o.unwrap_or(Self::DEFAULT_MAX_BUFFER);
-        let (sender, receiver) = channel();
-        let worker_handler = std::thread::spawn(move || {
-            let mut last_throughput = 0.;
-            let mut batch_size = Self::BLOCK_SIZE;
-            let mut batch = vec![];
-            loop {
-                batch.clear();
-                if batch.capacity() < batch_size * 2 {
-                    // avoid batch reallocation
-                    let min_buffer_size = max_buffer.min(batch_size * 2);
-                    let mut blocks = min_buffer_size / Self::BLOCK_SIZE;
-                    if min_buffer_size % Self::BLOCK_SIZE > 0 {
-                        blocks += 1;
-                    }
-                    batch.reserve(blocks * Self::BLOCK_SIZE);
-                }
+        let (batch_sender, mut batch_receiver) = mpsc::unbounded_channel();
+        let (io_sender, mut io_receiver) = mpsc::unbounded_channel();
 
-                let mut wakers = vec![];
-                let start = Instant::now();
+        let io_worker = std::thread::spawn(move || {
+            block_on(async move {
                 loop {
-                    match receiver.try_recv() {
+                    match io_receiver.recv().await {
+                        Some(Message::Exit) => break,
+                        Some(Message::Write {
+                            data,
+                            waker,
+                        }) => {
+                            file.write_all(&data).expect("write data failed");
+                            waker.send(()).expect("fail to wake batch worker")
+                        }
+                        _ => unreachable!("message sender cannot be dropped"),
+                    }
+                }
+            })
+        });
+
+        let io_sender_clone = io_sender.clone();
+        let worker_handler = std::thread::spawn(move || {
+            // let mut last_throughput = 0.;
+            let batch_size = 1024 * Self::BLOCK_SIZE;
+
+            let runtime = tokio::runtime::Runtime::new().expect("create runtime failed");
+            // let mut batch = vec![];
+            loop {
+                // batch.clear();
+                // if batch.capacity() < batch_size * 2 {
+                //     // avoid batch reallocation
+                //     let min_buffer_size = max_buffer.min(batch_size * 2);
+                //     let mut blocks = min_buffer_size / Self::BLOCK_SIZE;
+                //     if min_buffer_size % Self::BLOCK_SIZE > 0 {
+                //         blocks += 1;
+                //     }
+                //     batch.reserve(blocks * Self::BLOCK_SIZE);
+                // }
+                let mut wakers = vec![];
+                // let start = Instant::now();
+                let mut batch = Vec::with_capacity(batch_size);
+                loop {
+                    match batch_receiver.try_recv() {
                         Ok(Message::Exit) => return,
-                        Err(TryRecvError::Empty) => break,
+                        Err(mpsc::error::TryRecvError::Empty) => break,
                         Ok(Message::Write { data, waker }) => {
                             wakers.push(waker);
                             batch.extend_from_slice(&data);
-                            if batch.len() > batch_size {
+                            if batch.len() % Self::BLOCK_SIZE + 100 > Self::BLOCK_SIZE {
                                 break;
                             }
                         }
@@ -70,31 +98,43 @@ impl Logger {
                     }
                 }
 
-                file.write_all(&batch).expect("write data failed");
-                let throughput = batch.len() as f64 / start.elapsed().as_secs_f64();
-                let errs = (throughput - last_throughput) / last_throughput;
-                if errs >= 0.1 {
-                    batch_size *= 2;
-                } else if errs <= -0.1 {
-                    batch_size = batch_size * 3 / 4;
-                }
-                last_throughput = throughput;
-                for waker in wakers {
-                    waker.send(()).expect("Fail to wake log writer")
-                }
+                // file.write_all(&batch).expect("write data failed");
+                // let throughput = batch.len() as f64 / start.elapsed().as_secs_f64();
+                // let errs = (throughput - last_throughput) / last_throughput;
+                // if errs >= 0.1 {
+                //     batch_size *= 2;
+                // } else if errs <= -0.1 {
+                //     batch_size = batch_size * 3 / 4;
+                // }
+                // last_throughput = throughput;
+
+                let (io_waker, io_receiver) = oneshot::channel();
+                io_sender_clone.send(Message::Write {
+                    data: batch.into(),
+                    waker: io_waker,
+                }).expect("io sender cannot be dropped");
+
+                runtime.spawn(async move {
+                    io_receiver.await.expect("io receiver cannot be dropped");
+                    for waker in wakers {
+                        waker.send(()).expect("Fail to wake log writer")
+                    }
+                });
             }
         });
 
         Ok(Self {
-            sender,
-            worker_handler: Some(worker_handler),
+            batch_sender,
+            batch_worker: Some(worker_handler),
+            io_sender,
+            io_worker: Some(io_worker),
         })
     }
 
-    pub async fn write_log(&self, data: &[u8]) -> Result<()> {
+    pub async fn write_log(&self, data: Bytes) -> Result<()> {
         let (waker, receiver) = oneshot::channel();
-        let _ = self.sender.send(Message::Write {
-            data: data.to_vec(),
+        let _ = self.batch_sender.send(Message::Write {
+            data,
             waker,
         });
         receiver
@@ -103,8 +143,13 @@ impl Logger {
     }
 
     pub fn shutdown(&mut self) {
-        if let Some(handler) = self.worker_handler.take() {
-            let _ = self.sender.send(Message::Exit);
+        if let Some(handler) = self.batch_worker.take() {
+            let _ = self.batch_sender.send(Message::Exit);
+            handler.join().expect("Failed to join logger thread");
+        }
+
+        if let Some(handler) = self.io_worker.take() {
+            let _ = self.io_sender.send(Message::Exit);
             handler.join().expect("Failed to join logger thread");
         }
     }
--- src/test.rs
+++ src/test.rs
@@ -1,5 +1,7 @@
 use std::time::{Duration, Instant};
+use std::sync::Arc;
 
+use bytes::Bytes;
 use bytesize::ByteSize;
 use rand::RngCore;
 
@@ -7,6 +9,7 @@ use crate::Logger;
 
 #[tokio::test]
 async fn test_write_data() -> anyhow::Result<()> {
+    let guard = pprof::ProfilerGuardBuilder::default().frequency(1000).build()?;
     test_throughput_and_latency(1).await?;
     test_throughput_and_latency(10).await?;
     test_throughput_and_latency(100).await?;
@@ -15,34 +18,34 @@ async fn test_write_data() -> anyhow::Result<()> {
     test_throughput_and_latency(100_000).await?;
     test_throughput_and_latency(1000_000).await?;
     test_throughput_and_latency(10_000_000).await?;
+    if let Ok(report) = guard.report().build() {
+        let file = std::fs::File::create("flamegraph.svg")?;
+        report.flamegraph(file)?;
+    };
     Ok(())
 }
 
 async fn test_throughput_and_latency(task_size: usize) -> anyhow::Result<()> {
     const MSG_SIZE: usize = 100;
-    let guard = pprof::ProfilerGuardBuilder::default().frequency(1000).build()?;
-
-    let logger = Logger::open("test.log", None).unwrap();
+    let logger = Arc::new(Logger::open("test.log", None).unwrap());
     let mut rng = rand::thread_rng();
     let mut data = vec![0; MSG_SIZE];
 
     let mut tasks = Vec::with_capacity(task_size);
     rng.fill_bytes(&mut data);
+    let data_ref = Bytes::from(data);
     for _ in 0..tasks.capacity() {
-        tasks.push(async {
+        let data_ref_clone = data_ref.clone();
+        let logger_clone = logger.clone();
+        tasks.push(async move {
             let start = Instant::now();
-            logger.write_log(&data).await?;
+            logger_clone.write_log(data_ref_clone).await?;
             Ok::<_, anyhow::Error>(start.elapsed())
         });
     }
 
     let start = Instant::now();
     let results = futures::future::join_all(tasks).await;
-
-    if let Ok(report) = guard.report().build() {
-        let file = std::fs::File::create("flamegraph.svg")?;
-        report.flamegraph(file)?;
-    };
 
     let total_cost = start.elapsed();
     let avg_latency = results
